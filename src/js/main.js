536e933f5d80ea6696daf197d72d84418b9e65ff
rocioflo <rocioflorencioytal@gmail.com>
2022-07-15 21:21:04 +0200

    Add HTML structure, js classes, and bring them to js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//global variables
+const input = document.querySelector('.js_input');
+const submitBtn = document.querySelector('.js_submit_Btn');
+const resetBtn = document.querySelector('.js_reset_Btn');
+const resultsList = document.querySelector('.js_results_list');
+const favoritesList = document.querySelector('.js_favorites_list');
