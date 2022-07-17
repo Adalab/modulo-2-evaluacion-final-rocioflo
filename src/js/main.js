c006ac7cc47403625d99590ce8c65b4a5399f8db
rocioflo <rocioflorencioytal@gmail.com>
2022-07-17 11:21:20 +0200

    Add search features to js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('>> Ready :)');
-
 //global variables
 const input = document.querySelector('.js_input');
 const submitBtn = document.querySelector('.js_submit_Btn');
@@ -10,3 +8,46 @@ const resultsList = document.querySelector('.js_results_list');
 const favoritesList = document.querySelector('.js_favorites_list');
 
 //petition to server
+
+let animes = [];
+
+function handleInput(ev) {
+  ev.preventDefault();
+  const userInput = input.value.toLowerCase();
+
+  fetch(`https://api.jikan.moe/v4/anime?q=${userInput}`)
+    .then((response) => response.json())
+    .then((data) => {
+      animes = data.data;
+      console.log(animes);
+      renderAnime();
+    });
+}
+
+//image painter
+function renderAnime() {
+  for (const item of animes) {
+    if (
+      item.images.jpg.image_url ===
+      `https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png`
+    ) {
+      resultsList.innerHTML += `<li class="list-item js_list_item">${item.title} <img class="anime-image" src="https://via.placeholder.com/225x200/ffffff/666666/?text=sin%20imagen%20%20:("></li>`;
+    } else {
+      resultsList.innerHTML += `<li class="list-item js_list_item">${item.title} <img class="anime-image" src="${item.images.jpg.image_url}"></li>`;
+    }
+  }
+}
+
+console.log(animes);
+submitBtn.addEventListener('click', handleInput);
+
+//favs
+let animeFavs = [];
+
+function addFavs() {
+  const listItem = document.querySelector('.js_list_item');
+
+  listItem.addEventListener('click', (handleImageClick) =>
+    listItem.classList.add('red-background')
+  );
+}
